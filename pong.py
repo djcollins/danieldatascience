8a34748839d6f7d4d02a81ca2ac7396256757b47
Daniel Collins <daniel@v-f-g.com>
2020-11-14 18:19:32 +0200

    es working-ish

--- pong.py
+++ pong.py
@@ -13,7 +13,6 @@ class my_turtle():
         self.dy=dy
         self.x=x
         self.y=y
-
     def xcor(self):
         return self.x
     def ycor(self):
@@ -27,12 +26,14 @@ class my_turtle():
         self.y=y
 
 
-def get_turtle():
+def get_turtle(ball_speed):
+    wn = turtle.Screen()
+    wn.clear()
     paddle_a = turtle.Turtle()
     paddle_a.speed(0)
     paddle_a.shape("square")
     paddle_a.color("black")
-    paddle_a.shapesize(stretch_wid=1, stretch_len=5)
+    paddle_a.shapesize(stretch_wid=0.1, stretch_len=5)
     paddle_a.penup()
     paddle_a.goto(0, -250)
 
@@ -43,7 +44,7 @@ def get_turtle():
     paddle_b.shapesize(stretch_wid=1, stretch_len=5)
     paddle_b.penup()
     paddle_b.goto(250, 250)
-    wn = turtle.Screen()
+
     # wn.title("Ping Pong !!!")
     wn.bgcolor("white")
     wn.setup(width=800, height=600)
@@ -53,9 +54,9 @@ def get_turtle():
     ball.shape("circle")
     ball.color("black")
     ball.penup()
-    ball.goto(0, 0)
-    ball.dx = 0.6  # Can change the speed according to the speed of your system.
-    ball.dy = 0.6  # Can change the speed according to the speed of your system.
+    ball.goto(np.random.uniform(-350, 350), 0)
+    ball.dx = ball_speed # Can change the speed according to the speed of your system.
+    ball.dy = ball_speed  # Can change the speed according to the speed of your system.
     # Pen
     pen = turtle.Turtle()
     pen.speed(0)
@@ -86,19 +87,20 @@ def discount_rewards(r, gamma=0.99):
 
 
 
-def play_games(games_to_play, points_per_game,single_player=True ,show_moves=True,game_agent=None, save_moves=False, use_turtle=True):
+def play_games(games_to_play, points_per_game,ball_speed=200,single_player=True ,show_moves=True,game_agent=None, save_moves=False, use_turtle=True):
+    paddle_speed=ball_speed
     hit_reward = 1
     if(use_turtle==True):
-        paddle_a, paddle_b, ball, wn=get_turtle()
+        paddle_a, paddle_b, ball, wn=get_turtle(ball_speed)
     else:
         paddle_a=my_turtle(0,0,0,0)
         paddle_b=my_turtle(0,0,0,0)
         ball = my_turtle(0, 0, 0, 0)
         paddle_a.goto(0, -250)
         paddle_b.goto(250, 250)
-        ball.goto(0, 0)
-        ball.dx = 0.6
-        ball.dy = 0.6
+        ball.goto(np.random.uniform(-350, 350), 0)
+        ball.dx = ball_speed
+        ball.dy = ball_speed
     time_score = 0
     # Score
     score_a = 0
@@ -106,24 +108,20 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
     last_move=0
     # Paddle A
 
-    """
-    # Paddle B
-   
-    """
     # Ball
 
     def paddle_a_right():
         if (paddle_a.xcor() >= 350):
             return
         x = paddle_a.xcor()
-        x += 15
+        x += 50
         paddle_a.setx(x)
         #print("Paddle position {0} Ball x coord: {1} ball y coord: {2}".format((paddle_a.xcor(), paddle_a.ycor()),ball.xcor(), ball.ycor()))
     def paddle_a_left():
         if (paddle_a.xcor() <= -350):
             return
         x = paddle_a.xcor()
-        x -= 15
+        x -= 50
         paddle_a.setx(x)
         #print("Paddle position {0} Ball x coord: {1} ball y coord: {2}".format((paddle_a.xcor(), paddle_a.ycor()), ball.xcor(), ball.ycor()))
     # Keyboard bindings
@@ -145,25 +143,26 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
     score_a = 0
     score_b = 0
     bounce_counter=0
-    previous_frame=np.array(np.zeros(9))
+    previous_frame=np.array(np.zeros(3))
     st=time.time()
     game_in_progress=False
     last_move=0
     frame_counter=0
     # Main game loop
     if(game_agent==None):
-        updates_per_frame=20
+        updates_per_frame=5
     else:
-        updates_per_frame=10
+        updates_per_frame=5
     #print("Playing episode {} of {}".format(episode_count+1, episodes_to_play))
     for game_counter in range(games_to_play):
-        print("Playing Game {} of {} ".format(game_counter+1,games_to_play ))
+        #print("Playing Game {} of {} ".format(game_counter+1,games_to_play ))
         score_a = 0
         score_b = 0
         bounce_counter=0
         game_in_progress=True
         time_score=0
         while game_in_progress==True:
+
             frame_counter+=1
             # a*=-1
             # if(a==1):
@@ -173,14 +172,17 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
                 wn.update()
             #time_score+=0.1
             if(frame_counter%updates_per_frame==0): #only move and record the state every 10 frames
+                #print(frame_counter)
                 if(game_agent==None):
                     last_move=float(random.randint(0,1))
                 else:
-                    last_move=game_agent.predict(previous_frame.reshape(1,9))
-                    if(last_move>0.5):
-                        last_move=1
-                    else:
-                        last_move=0
+                    #print(previous_frame)
+                    last_move=game_agent.predict(previous_frame.reshape(1,3))
+                    #print(last_move)
+                    last_move=round(last_move[0][0])
+                    #print()
+                    #print(last_move)
+
                 if (last_move == 0):
                     paddle_a_left()
                 else:
@@ -201,11 +203,17 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
                     ball.dx *= -1
 
                 # Paddle and ball collisions
-                if ball.ycor() <= -250 and ball.xcor() < paddle_a.xcor() + 50 and ball.xcor() > paddle_a.xcor() - 50:
-                    ##print("We hit the ball! {0} Bounces".format(bounce_counter))             ###!!!
+
+                #calculate gradient of ball in case we "missed" but actually hit
+                m=(previous_frame[1]-ball.ycor()) / (previous_frame[0]-ball.xcor())
+                c=previous_frame[1]-m * previous_frame[0]
+                x=-250/m -c
+                if (ball.ycor() <= -250) and ((paddle_a.xcor()-50<x<paddle_a.xcor()+50) or  ( paddle_a.xcor() - 50<ball.xcor() < paddle_a.xcor() + 50)):
+                                 ###!!!
+                    bounce_counter += 1
                     ball_hits+=1
+                    #print("We hit the ball! {0} Bounces".format(bounce_counter))
                     ball.dy *= -1
-                    bounce_counter+=1
                     ball.sety(ball.ycor()+12)
                     rewards.append(1.0)
                 elif ball.ycor() < -255:
@@ -215,37 +223,38 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
                     # ball must start in a random position, or the network will just learn a pattern
                     ball.goto((random.random() * 350) - 175, 250)
                     ball.dx *= random.choice([1, -1])
-                    rewards.append(-1.0)
-                    ball_misses+=1
-                    # print("We missed the ball. Time score: {0}  Bounces: {1}".format(time_score, bounce_counter))    ###!!!
+                    #rewards.append(-1.0)
+                    #print("We missed the ball. Time score: {0}  Bounces: {1}".format(time_score, bounce_counter))    ###!!!
+                    paddle_a.setx(0)
+                    ball_misses += 1
                     bounce_counter = 0
                     time_score = 0
                     # print("Paddle position {0} Ball x coord: {1} ball y coord: {2}".format((paddle_a.xcor(), paddle_a.ycor()),
                     #                                                                      ball.xcor(), ball.ycor()))
-                else:
-                    rewards.append(0.0)
+                # else:
+                #     #rewards.append(0.0)
 
 
                 ball_x=ball.xcor()
                 ball_y=ball.ycor()
                 paddle_x=paddle_a.xcor()
                 paddle_y=paddle_a.ycor()
-                current_frame=np.array([ball_x, ball_y, paddle_x, paddle_y, ball_x-paddle_x, ball_y-paddle_y, ball_x-paddle_x + ball_y-paddle_y, last_move,1 if game_in_progress==True else 0])
+                current_frame=np.array([ball_x, ball_y, paddle_x]) # ball_x-paddle_x, ball_y-paddle_y, ball_x-paddle_x + ball_y-paddle_y, last_move,1 if game_in_progress==True else 0])
                 frame_stack.append(current_frame)
                 #print(current_frame)
                 #print(last_move)
                 #print(rewards[-1])
                 previous_frame=current_frame
-                if (score_a >= points_per_game or score_b >= points_per_game):
+                if (score_a >= points_per_game or score_b >= points_per_game or sum(rewards)>21):
                     game_in_progress = False
                     ### print("Game over ")               ###!!!
                     bounce_counter = 0
     #rewards=discount_rewards(rewards, 0.95)
-    print(len(rewards))
-    print(len(frame_stack))
-    print(len(moves))
-    print("Ball Misses {}".format(ball_misses))
-    print("Ball Hits {}".format(ball_hits))
+    #print(len(rewards))
+    #print(len(frame_stack))
+    #print(len(moves))
+    #print("Ball Misses {}".format(ball_misses))
+    #print("Ball Hits {}".format(ball_hits))
     if(save_moves==True):
         with open("move_array.npy", "wb") as f:
             np.save(f,np.array(moves, dtype=np.float64))
@@ -253,7 +262,7 @@ def play_games(games_to_play, points_per_game,single_player=True ,show_moves=Tru
             np.save(f,np.array(frame_stack, dtype=np.float64))
         with open("reward_array.npy", "wb") as f:
             np.save(f,np.array(rewards, dtype=np.float64))
-    print(time.time()-st)
+    #print(time.time()-st)
     return frame_stack, moves, rewards
 #a,b,c=play_games(100, 21, show_moves=False,use_turtle=False)
 # print(len(a[0]))
--- train_network.py
+++ train_network.py
@@ -1,106 +1,204 @@
-
-#using turtle for 100 games takes 37.4 seconds (14.1% hit rate using random moves)
-#using my own turtle class for 100 games takes 2.4 seconds (we use this for training and testing) (14.2% hit rate using random moves)
+# using turtle for 100 games takes 37.4 seconds (14.1% hit rate using random moves)
+# using my own turtle class for 100 games takes 2.4 seconds (we use this for training and testing) (14.2% hit rate using random moves)
 import random
 import time
+import collections
 import numpy as np
 import pong
 from tensorflow.keras import Sequential
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import *
 from tensorflow.keras.layers import Conv2D, Flatten, Dense
 
 
+# from openai
+
+def rank(x: np.ndarray):
+    """
+    Returns ranks in [0, len(x))
+    Note: This is different from scipy.stats.rankdata, which returns ranks in [1, len(x)].
+    """
+    assert x.ndim == 1
+    ranks = np.empty(len(x), dtype=int)
+    ranks[x.argsort()] = np.arange(len(x))
+    return ranks
+
+
+def centered_ranker(x: np.ndarray) -> np.ndarray:
+    y = rank(x.ravel()).reshape(x.shape).astype(np.float32)
+    y /= (x.size - 1)
+    y -= .5
+    return np.squeeze(y)
+
 
 def generate_random_moves_and_save():
-    frame_stack, moves, rewards=pong.play_games(10000, 21,single_player=True ,show_moves=False,game_agent=None, save_moves=True, use_turtle=False)
+    frame_stack, moves, rewards = pong.play_games(10000, 21, single_player=True, show_moves=False, game_agent=None,
+                                                  save_moves=True, use_turtle=False)
     print(len(frame_stack))
-generate_random_moves_and_save()
-def make_model_play(model_name, games,show_moves,save_moves=False):
-    model=load_model(model_name)
-    frames, moves, rewards = pong.play_games(games, 10, show_moves=show_moves, save_moves=save_moves,game_agent=model)
+
+
+# generate_random_moves_and_save()
+def make_model_play(model_name, games, show_moves, save_moves=False):
+    model = load_model(model_name)
+    frames, moves, rewards = pong.play_games(games, 10, show_moves=show_moves, save_moves=save_moves, game_agent=model,ball_speed=100, use_turtle=True)
+    print(sum(rewards))
     return frames, moves, rewards
-#make_model_play("model_saved", 10, False, False)
+#make_model_play("model_saved49", 10, True, False)
+
+# make_model_play("model_saved", 10, False, False)
 
 def train_model_with_feedback_save_n_times(model, n=5):
-    model_name=model
-    model=load_model(model)
+    model_name = model
+    model = load_model(model)
     for x in range(n):
-        frames, moves,rewards=make_model_play(model_name, 1000,show_moves=False)
-        print("misses {} hits{}".format(sum([x for x in rewards if x==-1]),sum([x for x in rewards if x==1])))
-        trained_model=train_model(model,frames,moves,rewards,20,200)
+        frames, moves, rewards = make_model_play(model_name, 1000, show_moves=False)
+        print("misses {} hits{}".format(sum([x for x in rewards if x == -1]), sum([x for x in rewards if x == 1])))
+        trained_model = train_model(model, frames, moves, rewards, 20, 200)
         model.save("model_".format((n), ))
-        model_name="model_"+str(n)
+        model_name = "model_" + str(n)
         print("SAVED MODEL" + str(n))
+
+
 def load_saved_data():
-    with open("frame_stack.npy","rb") as f:
-        frame_stack=np.load(f)
-    with open("moves.npy","rb") as f:
-        moves=np.load(f)
-    with open("rewards.npy","rb") as f:
-        rewards=np.load(f)
+    with open("frame_stack.npy", "rb") as f:
+        frame_stack = np.load(f)
+    with open("moves.npy", "rb") as f:
+        moves = np.load(f)
+    with open("rewards.npy", "rb") as f:
+        rewards = np.load(f)
     return frame_stack, moves, rewards
-def make_a_model(dim_inputs=9, layer_1_nodes=300, layer_2_nodes=50, output_nodes=1):
-    model=Sequential()
-    model.add(Dense(layer_1_nodes, input_dim=dim_inputs, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+
+
+def make_a_model(dim_inputs=4, layer_1_nodes=300, layer_2_nodes=50, output_nodes=1):
+    model = Sequential()
+    model.add(Dense(layer_1_nodes, input_dim=dim_inputs, activation='tanh'))
+    model.add(Dense(layer_2_nodes, activation='tanh'))
+    # model.add(Dense(10, activation='relu'))
     model.add(Dense(1, activation="sigmoid"))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
+
 def load_data_make_train_play():
-    frame_stack, moves, rewards=load_saved_data()
-    model=make_a_model(9,300,25, 1)
-    rewards=pong.discount_rewards(rewards, 0.95)
-    trained_model=train_model(model, frame_stack,moves,rewards,5,100)
-    pong.play_games(10, 21,single_player=True ,show_moves=True,game_agent=trained_model, save_moves=False)
+    frame_stack, moves, rewards = load_saved_data()
+    model = make_a_model(4, 300, 25, 1)
+    rewards = pong.discount_rewards(rewards, 0.95)
+    trained_model = train_model(model, frame_stack, moves, rewards, 5, 100)
+    pong.play_games(10, 21, single_player=True, show_moves=True, game_agent=trained_model, save_moves=False)
+
 
 def train_model(model, frame_stack, moves, rewards, epoch_count, batch_size_specified):
-    training_set_moves=[]
-    training_set_frames=[]
-    #get rid of moves and frames that won't benefit us
-    data=[]
+    training_set_moves = []
+    training_set_frames = []
+    # get rid of moves and frames that won't benefit us
+    data = []
     for x in range(len(rewards)):
-        if(rewards[x]>0.5):
-            #data.append([frame_stack[x]]+moves[x])
+        if (rewards[x] > 0.5):
+            # data.append([frame_stack[x]]+moves[x])
             training_set_frames.append(frame_stack[x])
             training_set_moves.append(moves[x])
-    training_set_frames=np.array(training_set_frames,dtype=float)
-    training_set_moves=np.array(training_set_moves, dtype=float)
-    training_set_moves=training_set_moves.reshape(training_set_moves.shape[0],1)
+    training_set_frames = np.array(training_set_frames, dtype=float)
+    training_set_moves = np.array(training_set_moves, dtype=float)
+    training_set_moves = training_set_moves.reshape(training_set_moves.shape[0], 1)
 
-    print("Frames dimensions {}".format((len(training_set_frames),training_set_frames[0].shape)))
-    #print(training_set_frames[1])
-    #print(training_set_frames[2])
-    #print(training_set_frames[3])
-    #print(training_set_frames[4])
+    print("Frames dimensions {}".format((len(training_set_frames), training_set_frames[0].shape)))
+    # print(training_set_frames[1])
+    # print(training_set_frames[2])
+    # print(training_set_frames[3])
+    # print(training_set_frames[4])
     print("Moves dimensions {}".format(training_set_moves.shape))
-    #print(training_set_moves)
+    # print(training_set_moves)
     print("Training set has {} items".format(len(training_set_moves)))
-    #good_moves=rewards[rewards>0.05]
-    #bad_moves=rewards[rewards<-0.05]
+    # good_moves=rewards[rewards>0.05]
+    # bad_moves=rewards[rewards<-0.05]
     print("model made")
     model.fit(training_set_frames, training_set_moves, epochs=epoch_count, batch_size=batch_size_specified)
     return model
+
+
 def make_train_and_return_model():
-    frame_stack, moves, rewards = pong.play_games(5000,21, show_moves=False, save_moves=True)
-    model=make_a_model(9, 50, 10, 1)
-    model=train_model(model, frame_stack, moves, rewards,10, 200 )
+    frame_stack, moves, rewards = pong.play_games(5000, 21, show_moves=False, save_moves=True)
+    model = make_a_model(4, 50, 10, 1)
+    model = train_model(model, frame_stack, moves, rewards, 10, 200)
     model.save("model_saved")
-    #print(frame_stack[1].reshape(1,9))
-    #print(model.predict(frame_stack[1].reshape(1,9)))
-    pong.play_games(10,20,game_agent=model)
-#make_train_and_return_model()
-def load_model_train_on_play_data_return_results(model_name,games_to_play=100):
-    model=load_model("model_saved")
-    frames, moves, rewards=load_saved_data()
-    model=train_model(model,frames,moves,rewards,10,3)
+    # print(frame_stack[1].reshape(1,9))
+    # print(model.predict(frame_stack[1].reshape(1,9)))
+    pong.play_games(10, 20, game_agent=model)
+
+
+# make_train_and_return_model()
+def load_model_train_on_play_data_return_results(model_name, games_to_play=100):
+    model = load_model("model_saved")
+    frames, moves, rewards = load_saved_data()
+    model = train_model(model, frames, moves, rewards, 10, 3)
     frames, moves, rewards = pong.play_games(10, 10, show_moves=False, save_moves=False,
                                              game_agent=model)
     return frames, moves, rewards
-def main():
-    #train_model_with_feedback_save_n_times("model_saved", n=5)
 
+
+def make_random_model():
+    model = Sequential()
+
+    model.add(Dense(64, input_dim=4, activation='relu'))
+    # model.add(Dense(50, activation='relu'))
+    # model.add(Dense(10, activation='relu'))
+    model.add(Dense(1, activation="sigmoid"))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def set_model_weights(model, weights):
+    ind = 0
+    for layer in model.layers:
+        layer_weights = layer.get_weights()[0]
+        # print(f"layer shape {layer.get_weights()[0].shape}")
+        layer.set_weights(
+            [np.reshape(weights[ind:layer_weights.size + ind], layer_weights.shape), layer.get_weights()[1]])
+        ind = layer_weights.size
+    return model
+
+
+def get_model_weights(model):
+    weights = []
+
+    for layer in model.layers:
+        # print(layer.get_weights()[0].shape)
+        weights.append(np.ravel(layer.get_weights()[0]))
+    return np.concatenate(weights)
+
+
+def main():
+    my_model = make_a_model(3, 64, 32)  # my main model
+    theta = get_model_weights(my_model)  # my main weights
+    std_dev = 0.02
+    lr = 0.1
+    generations = 500  # instaed of epoch/episode
+
+    n_trials = 50
+    for generation in range(generations):
+        print(f"Season {generation}")
+        noise = []
+        fitness = []
+
+        for n in range(n_trials):
+            if(n%20==0):
+                print(n)
+            noise.append(   np.random.randn(   len(theta) ) * std_dev)
+            set_model_weights(my_model, noise[-1] + theta)
+            frames, moves, rewards = pong.play_games(1, 1, game_agent=my_model, save_moves=False, use_turtle=False,
+                                                     show_moves=False)
+            fitness.append(sum(rewards))
+            #print(n, fitness, noise[-1])
+        ranked_fittness = centered_ranker(np.array(fitness, dtype=float))
+        print(f"Max fitness {max(fitness)} Average fitness {np.average(fitness)}")
+        weighted_average_noise = np.dot(ranked_fittness, noise)
+        theta += weighted_average_noise * lr
+        set_model_weights(my_model,theta)
+        my_model.save("model_saved"+str(generation))
+
+    print("awe")
+
+    # train_model_with_feedback_save_n_times("model_saved", n=5)
+    # frame_stack, moves, rewards = pong.play_games(21, 21, show_moves=True, save_moves=False, use_turtle=True)
     # play_random=True
     # decision_object=None
     # games_to_play=2000
@@ -108,20 +206,20 @@ def main():
     # single_player=True
     # frame_stack, moves, rewards=pong.play_games(games_to_play, points_per_game, show_moves=False)
 
-    #good_moves=rewards[rewards>0.05]
-    #bad_moves=rewards[rewards<-0.05]
+    # good_moves=rewards[rewards>0.05]
+    # bad_moves=rewards[rewards<-0.05]
 
-    #print(good_moves.size)
-    #print(bad_moves.size)
+    # print(good_moves.size)
+    # print(bad_moves.size)
     pass
+
 
 main()
 
-
 # def keep_learning()
-#load_data_make_train_play()
-#make_train_and_return_model()
-#main()
+# load_data_make_train_play()
+# make_train_and_return_model()
+# main()
 # frame_stack, moves, rewards=load_saved_data()
 # print(len(moves))
 # print(frame_stack[0])
